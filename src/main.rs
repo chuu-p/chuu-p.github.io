a06de738ea73188a8fe76eb737c3a2fa5fed9f27
chuu-p <chuu801@pm.me>
2025-03-13 09:49:11 +0100

    Implement error handling :broom:

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use blog::copy_static_content;
-use color_eyre::Report;
+use color_eyre::{eyre::OptionExt, Report};
 
 use crate::components::mastodon_comments::mastodon_comments;
 use crate::components::template::template;
-use core::fmt;
 use handlebars::{Handlebars, RenderError};
-use maud::{html, Markup, PreEscaped, Render};
+use maud::{html, Render};
 use pulldown_cmark::{html::push_html, Parser};
 use std::{
-    error::Error,
-    fmt::format,
-    fs::{self},
-    io,
+    fs::{self, DirEntry},
+    io::{self, ErrorKind},
     path::Path,
     sync::LazyLock,
 };
@@ -21,9 +18,11 @@ mod components;
 
 static REG: LazyLock<Handlebars<'static>> = LazyLock::new(Handlebars::new);
 
+/// On error handling: This is a program designed to be used via command line by
+/// a technical audience. This is why we bubble the errors to the main function
+/// and use `color_eyre::Report` to generate an error report on the console.
 fn main() -> Result<(), Report> {
     color_eyre::install()?;
-    // lets write some testable code!
 
     const OUT_DIR: &str = "docs";
     const PUBLIC_DIR: &str = "public";
@@ -34,7 +33,7 @@ fn main() -> Result<(), Report> {
     save_html_posts(&html_posts, OUT_DIR)?;
 
     save_file(
-        &template(component_index(html_posts)).into_string(),
+        &template(component_index(html_posts)?).into_string(),
         OUT_DIR,
         "index.html",
     )?;
@@ -50,20 +49,21 @@ fn main() -> Result<(), Report> {
     Ok(())
 }
 
-fn component_index(posts: Vec<(String, String)>) -> String {
+fn component_index(posts: Vec<(String, Table, String)>) -> Result<String, Report> {
     let links_contents = posts
         .into_iter()
-        .map(|(path, content)| (format!("/{}", path), content))
-        .collect::<Vec<(String, String)>>();
+        .map(|(path, config, content)| (format!("/{}", path), config, content))
+        .collect::<Vec<(String, Table, String)>>();
 
-    html! {
+    Ok(html! {
         h2 class="slogan" { "/blog" }
-        @for (link, content) in links_contents.into_iter() {
+        @for (link, config, _content) in links_contents.into_iter() {
             a href=(link) { (link) }
+            a { (config.get("title").ok_or_eyre(format!("Config Entry 'title' not present in {}", link))?) }
         }
     }
     .render()
-    .into_string()
+    .into_string())
 }
 
 fn save_file(content: &str, out_dir: &str, file_name: &str) -> io::Result<()> {
@@ -72,94 +72,66 @@ fn save_file(content: &str, out_dir: &str, file_name: &str) -> io::Result<()> {
     Ok(())
 }
 
-#[derive(Debug)]
-struct MyError {
-    details: String,
-}
-
-impl MyError {
-    fn new(msg: &str) -> MyError {
-        MyError {
-            details: msg.to_string(),
-        }
-    }
-}
-
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.details)
-    }
-}
-
-impl Error for MyError {
-    fn description(&self) -> &str {
-        &self.details
-    }
-}
-
-fn read_all_files(path: &str, extension: &str) -> Result<Vec<(String, String)>, std::io::Error> {
+fn read_all_files(path: &str, extension: &str) -> io::Result<Vec<(String, String)>> {
     let cwd = std::env::current_dir()?;
 
-    let entries: Vec<_> = fs::read_dir(cwd.join(path))
-        .unwrap()
+    let entries = fs::read_dir(cwd.join(path))?
         .filter_map(|res| {
-            res.and_then(|de| {
-                de.file_type().map(|ft| {
-                    (ft.is_file() && de.path().extension().is_some_and(|ext| ext == extension))
-                        .then_some(de)
-                })
-            })
-            .transpose()
+            let de = res.ok()?;
+            let ft = &de.file_type().ok()?;
+            if ft.is_file() && de.path().extension().is_some_and(|ext| ext == extension) {
+                Some(Ok::<std::fs::DirEntry, io::Error>(de))
+            } else {
+                None
+            }
         })
-        .collect::<Result<_, _>>()?;
+        .collect::<Result<Vec<DirEntry>, io::Error>>()?;
 
-    Ok(entries
+    entries
         .into_iter()
         .map(|de| {
             let path = cwd.join("src/content/").join(de.file_name());
             let mut html_path = path.clone();
             html_path.set_extension("html");
-            Ok::<(String, String), Box<dyn Error>>((
-                String::from(
-                    html_path.file_name()
-                        .ok_or_else(|| MyError::new("filname error"))?
-                        .to_str()
-                        .ok_or_else(|| MyError::new("to_str error"))?,
-                ),
-                fs::read_to_string(&path)?,
-            ))
+
+            let file_name = html_path
+                .file_name()
+                .ok_or_else(|| io::Error::new(ErrorKind::Other, "filename error"))?
+                .to_str()
+                .ok_or_else(|| io::Error::new(ErrorKind::Other, "to_str error"))?
+                .to_string();
+
+            let content = fs::read_to_string(&path)?;
+
+            Ok((file_name, content))
         })
-        .map(|x| {
-            println!("unwrapping {:?}", x);
-            x.unwrap()
-        })
-        // .filter_map(Result::ok)
-        .collect::<Vec<(String, String)>>())
+        .collect::<Result<Vec<(String, String)>, _>>()
 }
 
-fn render_posts_to_html(posts: &[(String, String)]) -> Result<Vec<(String, String)>, RenderError> {
+fn render_posts_to_html(
+    posts: &[(String, String)],
+) -> Result<Vec<(String, Table, String)>, Report> {
     let res = posts
         .iter()
         .map(|(path, post)| {
-            let (table, content) = get_header(post.to_string());
+            let (table, content) = get_header(post.to_string())?;
 
             let html = Markdown(&content).render();
             let out = BlogPost(&html).render(&table);
 
-            let rendered_page = REG.render_template(&out, &table)?;
+            let rendered_page = REG.render_template(&out?, &table)?;
 
             let rendered_page = template(rendered_page).into_string();
 
-            Ok::<(String, String), Box<dyn Error>>((path.to_owned(), rendered_page))
+            Ok::<(String, Table, String), Report>((path.to_owned(), table, rendered_page))
         })
-        .filter_map(Result::ok)
-        .collect::<Vec<(String, String)>>();
+        .collect::<Result<Vec<(String, Table, String)>, Report>>();
 
-    Ok(res)
+    res
 }
 
-fn save_html_posts(posts: &[(String, String)], output_dir: &str) -> io::Result<()> {
-    for (path, page) in posts {
+fn save_html_posts(posts: &[(String, Table, String)], output_dir: &str) -> io::Result<()> {
+    for (path, _config, page) in posts {
         let mut target = Path::new(output_dir).join(path);
         target.set_extension("html");
         fs::write(&target, page)?;
@@ -223,7 +195,7 @@ impl Markdown<'_> {
 struct BlogPost<'a>(&'a str);
 
 impl BlogPost<'_> {
-    fn render(self, config: &Table) -> String {
+    fn render(self, config: &Table) -> Result<String, RenderError> {
         let html_template = html! (
             div style="float:right; max-width: 25%;" {
                 blockquote {
@@ -235,17 +207,16 @@ impl BlogPost<'_> {
             }
         )
         .into_string();
-        let mut html = Handlebars::new()
-            .render_template(&html_template, &config)
-            .unwrap();
+        let mut html = Handlebars::new().render_template(&html_template, &config)?;
         let comments = mastodon_comments(config).into_string();
         html.push_str(self.0);
         html.push_str(&comments);
-        html
+        Ok(html)
     }
 }
 
-fn get_header(content: String) -> (Table, String) {
+// FIXME
+fn get_header(content: String) -> Result<(Table, String), Report> {
     let start_delimiter = "+++";
     let end_delimiter = "+++";
 
@@ -256,17 +227,17 @@ fn get_header(content: String) -> (Table, String) {
             let toml_str = &content[toml_start..toml_end];
 
             println!("converting {:?}", toml_str);
-            let toml: Table = toml::from_str(toml_str).unwrap();
+            let toml: Table = toml::from_str(toml_str)?;
             println!("toml {:?}", &toml);
 
             let markdown_start = toml_end + end_delimiter.len();
             let markdown_content = content[markdown_start..].to_string();
 
-            (toml, markdown_content)
+            Ok((toml, markdown_content))
         } else {
-            (Table::new(), content) // Handle missing end delimiter
+            Ok((Table::new(), content)) // Handle missing end delimiter
         }
     } else {
-        (Table::new(), content) // Handle missing start delimiter
+        Ok((Table::new(), content)) // Handle missing start delimiter
     }
 }
